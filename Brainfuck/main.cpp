9894f891ba4147bf1b08624d70f03be3d3d6fce9
CodyIsAwesome <cchilder@ucsc.edu>
2016-05-28 16:32:26 -0700

    Improved usage info, added a flag
    The flag dumps compiled source from a .mb file out to disk as it is
    being compiled, before it is run.

--- Brainfuck/main.cpp
+++ Brainfuck/main.cpp
@@ -6,6 +6,14 @@
 #include "Brainfuck.h"
 #include "MindBlown.h"
 
+//cut off the .mb extension, append .bf, return
+std::string Rename(std::string name)
+{
+	name = name.substr(name.length() - 3);
+	name += ".bf";
+	return name;
+}
+
 bool EndsWith(std::string const &fullString, std::string const &ending) {
 	if (fullString.length() >= ending.length()) 
 	{
@@ -19,9 +27,11 @@ bool EndsWith(std::string const &fullString, std::string const &ending) {
 
 void PrintUsage()
 {
-	std::cout << "Usage: brainfuck[.exe] filename" << std::endl;
-	std::cout << "If filename ends with .b or .bf, it will be run as brainfuck source." << std::endl;
-	std::cout << "If filename ends with .mb, it will be compiled as mindblown source, then run." << std::endl;
+	std::cout << "Usage: brainfuck [-b] filename" << std::endl;
+	std::cout << "If filename ends with .b or .bf, it will be run as Brainfuck source." << std::endl;
+	std::cout << "If filename ends with .mb, it will be compiled as Mind Blown source, then run." << std::endl;
+	std::cout << "If the -b flag is given, it will dump the compiled Brainfuck source from a Mind Blown file before running it" << std::endl;
+	std::cout << "-b means nothing if the original source was brainfuck." << std::endl;
 	std::cout << "For documentation, please view readme at https://github.com/CodyIsAwesome/Brainfuck" << std::endl;
 }
 
@@ -35,7 +45,14 @@ int main(int argc, char** argv)
 	}
 	else //parse args as filenames
 	{
-		std::string filename(argv[1]);
+		int filenamePos = 1;
+		bool dumpCompiledBrainfuck = false;
+		if (std::string(argv[1]).compare("-b") == 0)
+		{
+			filenamePos = 2;
+			dumpCompiledBrainfuck = true;
+		}
+		std::string filename(argv[filenamePos]);
 		std::ifstream file;
 		file.open(filename, std::ifstream::in);
 		if (file.is_open())
@@ -49,11 +66,25 @@ int main(int argc, char** argv)
 			}
 			else if (EndsWith(filename, ".mb"))
 			{
-				Run(Compile(buffer.str()));
+				if (dumpCompiledBrainfuck)
+				{
+					std::string bf = Compile(buffer.str());
+					std::ofstream outfile;
+					outfile.open(Rename(filename));
+					outfile << bf;
+					outfile.close();
+					Run(bf);
+				}
+				else
+				{
+					Run(Compile(buffer.str()));
+				}
 			}
 			else
 			{
-
+				std::cout << "Unrecognized file format: " + filename << std::endl;
+				std::cout << "Expecting filenames that end in .b, .bf, or .mb" << std::endl;
+				exit(1);
 			}
 		}
 		else
