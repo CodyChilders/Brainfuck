fa964c465595d7ca11b41f7fe62104dc1e0e1b8d
CodyIsAwesome <cchilder@ucsc.edu>
2016-05-28 16:17:55 -0700

    Cleaned up main
    It now won't run from stdin, but accepts files as args.  It prints usage
    information if no args were given.

--- Brainfuck/main.cpp
+++ Brainfuck/main.cpp
@@ -1,60 +1,65 @@
 //https://en.wikipedia.org/wiki/Brainfuck
 #include <iostream>
-#include <string>
-#include <vector>
 #include <cstdlib>
 #include <fstream>
 #include <sstream>
 #include "Brainfuck.h"
 #include "MindBlown.h"
 
-//change to 1 for Windows, 0 for Unix
-#define WINDOWS 1
+bool EndsWith(std::string const &fullString, std::string const &ending) {
+	if (fullString.length() >= ending.length()) 
+	{
+		return (0 == fullString.compare(fullString.length() - ending.length(), ending.length(), ending));
+	}
+	else 
+	{
+		return false;
+	}
+}
+
+void PrintUsage()
+{
+	std::cout << "Usage: brainfuck[.exe] filename" << std::endl;
+	std::cout << "If filename ends with .b or .bf, it will be run as brainfuck source." << std::endl;
+	std::cout << "If filename ends with .mb, it will be compiled as mindblown source, then run." << std::endl;
+	std::cout << "For documentation, please view readme at https://github.com/CodyIsAwesome/Brainfuck" << std::endl;
+}
 
 int main(int argc, char** argv)
 {
 	
 	if (argc == 1) //no options, stdin
 	{
-		std::cout << "Enter your brainfuck code, and enter EOF ";
-#if WINDOWS
-		std::cout << "(Ctrl + Z)";
-#else
-		std::cout << "(Ctrl + D)";
-#endif
-		std::cout << ":\n";
-		std::string line;
-		std::string allCode;
-		while (std::getline(std::cin, line))
-		{
-			allCode += line;
-		}
-		std::cin.ignore(0x7fffffff); //flush the EOF left in the stream from entry
-		//Run(allCode);
-		Run(Compile(allCode));
-		int breakhere = 0;	
+		PrintUsage();
+		exit(0);
 	}
-	/*
 	else //parse args as filenames
 	{
-		for (int i = 1; i < argc; i++)
+		std::string filename(argv[1]);
+		std::ifstream file;
+		file.open(filename, std::ifstream::in);
+		if (file.is_open())
 		{
-			std::string filename(argv[i]);
-			std::ifstream file;
-			file.open(filename, std::ifstream::in);
-			if (file.is_open())
+			std::stringstream buffer;
+			buffer << file.rdbuf();
+			std::cout << "Output for " << filename << ":\n";
+			if (EndsWith(filename, ".b") || EndsWith(filename, ".bf"))
 			{
-				std::stringstream buffer;
-				buffer << file.rdbuf();
-				std::cout << "Output for " << filename << ":\n";
 				Run(buffer.str());
+			}
+			else if (EndsWith(filename, ".mb"))
+			{
+				Run(Compile(buffer.str()));
 			}
 			else
 			{
-				std::cout << std::endl << "Error opening file " << filename << std::endl << std::endl;
+
 			}
-			file.close();
 		}
+		else
+		{
+			std::cout << std::endl << "Error opening file " << filename << std::endl << std::endl;
+		}
+		file.close();
 	}
-	*/
 }
